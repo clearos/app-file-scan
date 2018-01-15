ec4f26ad92fff605dca5c6e35e33a9affe256559
pcbaldwin <peter@egloo.ca>
2018-01-15 08:46:58 -0500

    Bumped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'file_scan';
-$app['version'] = '2.1.7';
+$app['version'] = '2.4.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
